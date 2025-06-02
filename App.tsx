26f30ddd6e3a0c22c85106b1cb7d46a39fb6469d
Alexis CHAPPRON <bullrot-10@hotmail.fr>
2025-06-02 08:51:51 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RNMap from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <RNMap/>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ README.md
@@ -0,0 +1,8 @@
+This repo is a simple reproduciton of
+https://github.com/react-native-maps/react-native-maps/issues/5492
+
+It uses expo.
+`npx create-expo-app@latest --template expo-template-blank-typescript`
+
+
+When running `npm run typecheck`, tsc reports some errors coming from react-native-maps itself 
